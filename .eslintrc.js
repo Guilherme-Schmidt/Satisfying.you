a3de9e7015b3c74aa36e137b21a6abb9d546389c
Guilherme Schmidt <guischmidt201@gmail.com>
2024-10-23 11:36:33 -0300

    login

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native-community', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ['react'],
+  rules: {
+    // Regras personalizadas, se necessário
+  },
 };
