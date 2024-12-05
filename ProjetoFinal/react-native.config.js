5ced571340ed2d878b07e53ffb2abee555149f72
Guilherme Schmidt <devguilhermeschmidt@gmail.com>
2024-12-05 15:05:41 -0300

    atualizado

--- ProjetoFinal/react-native.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    assets: ['./assets/fonts'],
-    assets: ['./node_modules/react-native-vector-icons/Assets'], // Isso aponta para a pasta onde suas fontes estão armazenadas
-  };
